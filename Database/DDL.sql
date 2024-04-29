9713af69207eb85eda8741f86a9743619489fee3
Matheus Queiroz <87786441+madmattp@users.noreply.github.com>
2024-04-29 04:25:13 -0300

    [BACKEND] Update DDL.sql

--- Database/DDL.sql
+++ Database/DDL.sql
@@ -11,21 +11,21 @@ GRANT ALL PRIVILEGES ON `db_asset_hub`.* TO 'asset_hub_db_user'@'%';
 CREATE TABLE IF NOT EXISTS `db_asset_hub`.`tb_passwd` (
   `username` VARCHAR(25) NOT NULL,
   `name` VARCHAR(100) NULL,
-  `hash` VARCHAR(45) NOT NULL,
+  `user_hash` VARCHAR(400) NOT NULL,
   `enabled` TINYINT NOT NULL,
   PRIMARY KEY (`username`));
-
-
 
 CREATE TABLE IF NOT EXISTS `db_asset_hub`.`tb_ambiente` (
   `ambiente` VARCHAR(45) NOT NULL,
   `description` VARCHAR(240) NULL,
   PRIMARY KEY (`ambiente`));
 
+
 CREATE TABLE IF NOT EXISTS `db_asset_hub`.`tb_orgs` (
   `organization` VARCHAR(45) NOT NULL,
   `description` VARCHAR(240) NULL,
   PRIMARY KEY (`organization`));
+
 
 CREATE TABLE IF NOT EXISTS `db_asset_hub`.`tb_hardware` (
   `hardware` VARCHAR(45) NOT NULL,
@@ -33,7 +33,7 @@ CREATE TABLE IF NOT EXISTS `db_asset_hub`.`tb_hardware` (
   PRIMARY KEY (`hardware`));
 
 CREATE TABLE IF NOT EXISTS `db_asset_hub`.`tb_site` (
-  `site` VARCHAR(45) NOT NULL,
+  `site` INT NOT NULL,
   `description` VARCHAR(240) NULL,
   PRIMARY KEY (`site`));
 
@@ -56,13 +56,21 @@ CREATE TABLE IF NOT EXISTS `db_asset_hub`.`tb_host` (
   INDEX `fk_tb_host_tb_site1_idx` (`tb_site_site` ASC) VISIBLE,
   CONSTRAINT `fk_tb_host_tb_ambiente1`
     FOREIGN KEY (`tb_ambiente_ambiente`)
-    REFERENCES `db_asset_hub`.`tb_ambiente` (`ambiente`),
+    REFERENCES `db_asset_hub`.`tb_ambiente` (`ambiente`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_tb_host_tb_orgs1`
     FOREIGN KEY (`tb_orgs_organization`)
-    REFERENCES `db_asset_hub`.`tb_orgs` (`organization`),
+    REFERENCES `db_asset_hub`.`tb_orgs` (`organization`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_tb_host_tb_hardware1`
     FOREIGN KEY (`tb_hardware_hardware`)
-    REFERENCES `db_asset_hub`.`tb_hardware` (`hardware`),
+    REFERENCES `db_asset_hub`.`tb_hardware` (`hardware`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
   CONSTRAINT `fk_tb_host_tb_site1`
     FOREIGN KEY (`tb_site_site`)
-    REFERENCES `db_asset_hub`.`tb_site` (`site`));
+    REFERENCES `db_asset_hub`.`tb_site` (`site`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
