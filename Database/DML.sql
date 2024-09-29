626c12902d6cccb626489533a390335ab5bc362a
Matheus Queiroz <87786441+madmattp@users.noreply.github.com>
2024-09-29 18:10:24 -0300

    Update DML.sql

--- Database/DML.sql
+++ Database/DML.sql
@@ -1,3 +1,5 @@
 USE `db_asset_hub`;
 
 INSERT INTO tb_passwd (username, user_hash, enabled) values ('admin', 'cbae5c1e505b8b0fcaf02b41546cd80df824532af0c97bfc0732a35463c45ea5', 1);
+
+INSERT INTO tb_host_list (id, hosts) VALUES (1, '[]');
